9e1d7d5137c5ea3ee807f923d8aa082fd610786f
MrMilul <miladmizani@yahoo.com>
2021-01-15 21:10:21 +0200

    backend improved

--- backend/leadmanager/urls.py
+++ backend/leadmanager/urls.py
@@ -1,8 +1,9 @@
-
 from django.contrib import admin
 from django.urls import path, include
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('leads/', include('leads.urls'))
+    path('', include('frontend.urls')),
+    path('', include('leads.urls'))
 ]
