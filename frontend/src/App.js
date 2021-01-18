efa250918ece17daf3b72aff8167570335c85d3c
MrMilul <miladmizani@yahoo.com>
2021-01-18 20:42:39 +0200

    implementing the react-alert Provider

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,40 @@
 import React, { Component, Fragment } from 'react'
 import Header from './components/Header'
 import { Provider } from 'react-redux'
+import {Provider as AlertProvider } from 'react-alert'
+import AlertTemplate from 'react-alert-template-basic'
 
 import store from './store'
 import Dashboard from './components/lead/Dashboard'
+import Alerts from './components/layout/Alerts' 
+
+export const types = {
+  INFO: 'info',
+  SUCCESS: 'success',
+  ERROR: 'error'
+}
+
+const alertOptions = {
+  timeout: 3000, 
+  position: 'top center', 
+  offset: "10px", 
+  type: types.error
+}
+
 
 export default class App extends Component {
   render() {
     return (
       <Provider store={store} >
-        <Fragment>
-          <Header/>
-          <div className="container">
-          <Dashboard/>
-          </div>
-        </Fragment>
+        <AlertProvider template={ AlertTemplate } {...alertOptions}>
+          <Fragment>
+            <Header/>
+            <Alerts/>
+            <div className="container">
+            <Dashboard/>
+            </div>
+          </Fragment>
+        </AlertProvider>
       </Provider>
     )
   }
