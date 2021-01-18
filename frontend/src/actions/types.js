bdbf628fde008c9fde01684b1f661382353837fc
MrMilul <miladmizani@yahoo.com>
2021-01-18 20:42:06 +0200

    code the error action

--- frontend/src/actions/types.js
+++ frontend/src/actions/types.js
@@ -1,6 +1,8 @@
 export const GET_LEADS = 'GET_LEADS'
 export const DELETE_LEAD = 'DELETE_LEAD'
 export const POST_LEAD = "POST_LEAD"
+export const GET_ERRORS = "GET_ERRORS"
+
 
 export const FETCH_LEADS_DATA = (data) => {
     return {
@@ -20,5 +22,12 @@ export const POST_LEAD_DATA = (data) =>{
     return {
         type:POST_LEAD, 
         payload: data
+    }
+}
+
+export const GET_ERRORS_DATA = (err)=>{
+    return {
+        type: GET_ERRORS, 
+        payload: err
     }
 }
