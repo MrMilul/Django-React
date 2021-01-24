f4a9ab2c6df7a66f37a243ddbba08f4da401e4d3
MrMilul <miladmizani@yahoo.com>
2021-01-24 16:55:57 +0200

    account serialiazers

--- backend/accounts/api.py
+++ backend/accounts/api.py
@@ -7,12 +7,14 @@ class RegisterAPI(generics.GenericAPIView):
     serializer_class = RegisterSerializer
 
     def post(self, request, *args, **kwargs):
-        ser = self.get_serializer(data=request)
+        ser = self.get_serializer(data=request.data)
         ser.is_valid(raise_exception=True)
         user = ser.save()
+
+        token = AuthToken.objects.create(user)[1]
 
         return Response({
             'user': UserSerializer(user,
              context=self.get_serializer_context()).data, 
-            'token': AuthToken.objects.create(user)
+            'token': token
         })
